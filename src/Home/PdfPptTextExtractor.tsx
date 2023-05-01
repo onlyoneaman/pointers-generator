7598dd53a1e8154fd64763e308d1fce7179401a4
Aman <2000.aman.sinha@gmail.com>
2023-05-02 02:19:10 +0530

    design

--- src/Home/PdfPptTextExtractor.tsx
+++ src/Home/PdfPptTextExtractor.tsx
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import darkLogo from '../../assets/dark_mode.png';
-import lightLogo from '../../assets/light_mode.png';
-import {Simulate} from "react-dom/test-utils";
-import load = Simulate.load;
+
+import darkLogo from '../commons/logo-dark.png';
+import lightLogo from '../commons/logo-light.png';
 
 const API_KEY = import.meta.env.VITE_OPENAI_API_KEY;
 
@@ -46,25 +45,39 @@ function GptSummaryGenerator() {
 
   return (
     <div
-      className={`${dark?"dark":""} h-[95vh] w-screen`}
+      className={`${dark?"dark":""} h-[90vh] w-full`}
     >
       <div
         className="flex flex-wrap min-w-full h-full"
       >
         <div
-          className={"bg-primary text-secondary dark:bg-secondary dark:text-primary p-2 cursor-pointer flex justify-between min-w-full"}
+          className={"bg-primary align-middle text-secondary dark:bg-secondary dark:text-primary py-6 px-4 border-b-2 border-secondary dark:border-primary cursor-pointer flex justify-between items-center min-w-full"}
         >
+          <img
+            src={dark? lightLogo : darkLogo}
+            alt="logo"
+            className="h-10"
+          />
           <div
-            className={""}
-            onClick={() => setDark(!dark)}
+            className={"flex items-center"}
           >
+            <div
+              className={"bg-secondary uppercase font-bold text-primary px-4 py-2 mr-4 rounded cursor-pointer dark:bg-primary dark:text-secondary"}
+            >
+              Buy me a coffee
+            </div>
+            <div
+              className={"h-8"}
+              onClick={() => setDark(!dark)}
+            >
             <span className="material-symbols-outlined">
               {dark ? "light_mode" : "dark_mode"}
             </span>
+            </div>
           </div>
         </div>
-        <div className="flex flex-wrap w-screen h-full">
-          <div className="flex-1 h-full p-6 bg-primary text-secondary dark:bg-secondary dark:text-primary">
+        <div className="flex flex-col sm:flex-row flex-wrap w-screen h-full">
+          <div className="flex-1 h-full p-4 sm:p-6 bg-primary text-secondary dark:bg-secondary dark:text-primary">
             <form
               onSubmit={handleSubmit}
               className={"h-full flex flex-col justify-between"}
@@ -76,7 +89,7 @@ function GptSummaryGenerator() {
                 placeholder="Enter your script or data here..."
                 autoFocus
                 required
-                rows={20}
+                rows={10}
               />
               <button
                 className="px-4 mt-0 py-2 uppercase w-full text-lg font-bold text-white bg-black dark:bg-primary dark:text-secondary"
@@ -91,7 +104,7 @@ function GptSummaryGenerator() {
               </button>
             </form>
           </div>
-          <div className="flex-1 h-full p-6 bg-primary text-secondary dark:bg-secondary dark:text-primary">
+          <div className="flex-1 h-full p-4 sm:p-6 bg-primary text-secondary dark:bg-secondary dark:text-primary">
             <div
               className="h-full overflow-y-auto flex flex-col justify-between"
             >
@@ -102,7 +115,7 @@ function GptSummaryGenerator() {
                 {summary?.trim()}
               </p>
               <div
-                className="px-4 mt-0 py-2 uppercase w-full text-lg font-bold text-white bg-black cursor-pointer text-center dark:bg-primary dark:text-secondary"
+                className={`px-4 mt-0 py-2 uppercase w-full text-lg font-bold text-white bg-black cursor-pointer text-center dark:bg-primary dark:text-secondary ${summary?.trim() ? "" : "text-[#86856B] dark:text-[#999]"}`}
                 onClick={() => navigator.clipboard.writeText(summary)}
               >
                 Copy

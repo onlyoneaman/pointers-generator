4f95a5133aca5e772a714a77391906d599a7f616
Aman Kumar <onlyoneaman>
2023-05-19 17:24:38 +0530

    Add Pointers - Prompt

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ function App(): ReactElement {
   return (
     <div>
       Down Currently.
+      Use this prompt to generate pointers in ChatGPT
+      <br />
+      You are an AI assistant. Your client is a busy professional who needs a summary of the script. Provide a summary of the script in at least 10 points.
     </div>
   )
 
